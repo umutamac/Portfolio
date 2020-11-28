acbe3bdc0eab4fbd0cc91c0e7fe6c6c4baa46127
Umut Alptekin <amacalptekin@gmail.com>
2020-11-28 21:54:36 +0300

    deleted 'children' from Row, Container, Col inputs, to solve ...Not valid as React child (found abject with keys {children})

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ function App() {
     return (
         <Router>
             <div>
-                <Nav>
-                    
-                </Nav>
+                <Nav></Nav>
 
                 <Switch>
                     <Route exact path="/" component={About} />
--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -2,24 +2,20 @@ import React from "react";
 
 // Exporting the Container, Row, and Col components from this file
 
-export function Container( children ) {
-  return <div className="row">{children}</div>;
+export function Container() {
+  return <div className="row"></div>;
 }
 
-export function Row( children ) {
-  return <div className="row">{children}</div>;
+export function Row() {
+  return <div className="row"></div>;
 }
 
 // This Col component lets us size materialize columns with less syntax
 // e.g. <Col size="m12"> instead of <div className="col m12">
-export function Col({ size, children }) {
+export function Col({ size}) {
   return (
-    <div
-      className={size
-        .trim()
-        .map(size => "col " + size)}
-    >
-      {children}
+    <div className={size.trim().map(size => "col " + size)} >
+
     </div>
   );
 }

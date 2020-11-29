6248a4dacb6ad1cc8d8579c932922786f2358d5a
Umut Alptekin <amacalptekin@gmail.com>
2020-11-29 20:20:18 +0300

    adjusted paths of links in nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -9,13 +9,13 @@ function Nav() {
         <a className="dropdown-trigger left-align col s4" href="#!" data-target="dropdown1">
           <i className="material-icons hoverable">menu</i></a>
         <ul id="dropdown1" className="dropdown-content">
-          <li><Link to="/">About Me</Link></li>
-          <li><Link to="/portfolio">Portfolio</Link></li>
+          <li><Link to="/react-portfolio">About Me</Link></li>
+          <li><Link to="/react-portfolio/portfolio">Portfolio</Link></li>
           {/* <li className="active"><a href="index.html">About</a></li>
           <li><a href="more-html/portfolio.html">Portfolio</a></li> */}
         </ul>
         
-        <Link to="/" className="center-align col s8 hoverable" href="index.html" id="name">
+        <Link to="/" className="center-align col s8 hoverable" id="name">
           <span className="flow-text">Umut A. ALPTEKIN</span>
         </Link>
       </div>

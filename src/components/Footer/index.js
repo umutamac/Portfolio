32607d6329e2b532f981c9dc08a3f34dfb811796
Umut Alptekin <amacalptekin@gmail.com>
2020-12-03 15:06:19 +0300

    changed background img, footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,22 +2,32 @@ import React from "react";
 import "./style.css";
 
 function Footer() {
-  return (
-    <footer className="page-footer valign-wrapper">
-        <div className="footer-left">
-            <p>If you have any questions, or just want to say hello, feel free to contact me. Here are my social pages:
-            </p>
-        </div>
-        <div className="icons-container-right flex-container">
-            <a href="https://www.linkedin.com/in/umutalptekin" target="_blank" rel="noopener noreferrer">
-                <div className="icon icon-linkedin hoverable"></div>
-            </a>
-            <a href="https://github.com/umutamac" target="_blank" rel="noopener noreferrer">
-                <div className="icon icon-github hoverable" id="github-icon"></div>
-            </a>
-        </div>
-    </footer>
-  );
+    return (
+        <footer className="page-footer valign-wrapper">
+            <div className="contact valign-wrapper">
+                <div className="contact-left">
+                    <p>If you have any questions, or just want to say hello, you can always reach out to me using:
+                    </p>
+                </div>
+                <div className="icons-container-right flex-container">
+                    <a href="https://www.linkedin.com/in/umutalptekin" target="_blank" rel="noopener noreferrer">
+                        <div className="icon icon-linkedin hoverable"></div>
+                    </a>
+                    <a href="https://github.com/umutamac" target="_blank" rel="noopener noreferrer">
+                        <div className="icon icon-github hoverable"></div>
+                    </a>
+                    <a href="mailto:amacalptekin@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <div className="icon icon-email hoverable"></div>
+                    </a>
+                </div>
+            </div>
+            <div id="attributeArea">
+                <a href="https://www.freepik.com/vectors/background" target="_blank" rel="noopener noreferrer">
+                    Background vector created by pikisuperstar - www.freepik.com
+                </a>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;

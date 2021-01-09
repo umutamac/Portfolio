c6f0a4307d56e7a3364992d74fbc0f23dbba95b9
Umut Alptekin <amacalptekin@gmail.com>
2021-01-10 02:04:23 +0300

    bio section is made into a component

---
+++ src/components/Bio/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.css";
+
+function Bio() {
+
+    return (
+        <div id="about-text">
+            <p>Hello there! My name is Umut Alptekin. I am currently looking for a job as either an engineer, or a Full-Stack/Back-end developer.
+          </p>
+
+            <p>I have recently graduated from Penn State University with a Bachelor of Science in Mechanical Engineering in May 2020,
+            and, shortly after, finished a 3-month long bootcamp about Full-Stack Web Development from University of Texas at Austin.
+          </p>
+
+            <p>For some background information, I was born and raised in Istanbul, Turkey. I have gone to Lycée Saint-Joseph Istanbul, a French high school, then moved to USA to attend Penn State University.
+            Currently, I am back in Istanbul for the foreseeable future, but still open to relocating to another country to chase a work opportunity.
+          </p>
+
+            <p>The bootcamp was a very interesting and constructive experience for me. I have learned so much about coding in general, project workflow in software, and about web development itself.
+            Every week, I was challenged with a new project about a new topic, and had to learn how to use the tools I was just introduced to pretty much on the job.
+            After the bootcamp, I am still learning more about the tools and technologies to better leverage them, and finding different solutions to given problems.
+          </p>
+
+            <p>While in Penn State, I loved studying engineering, since the technical problem-solving aspect gives me quite a lot of joy.
+            One of the most memorable things I was involved in was my capstone project where my teammates and I were building a gas-powered, fiberglass car with the maximum fuel efficiency as possible.
+            Our plans to use it in the Shell Eco-marathon competition could not happen, it was still an amazing experience.
+          </p>
+        </div>
+    );
+}
+
+export default Bio;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./about.css";
 
+import Bio from "../components/Bio";
+import { Row, Col } from "../components/Grid";
+
 import headshot from "../assets/aboutPics/Alptekin_U_01.jpg";
 import psuLogo from "../assets/aboutPics/psu_coe.png";
 import bootcamp from "../assets/aboutPics/UT_bootcamp.png";
 import sj from "../assets/aboutPics/sj.jpg";
-import { Row, Col } from "../components/Grid";
+
 
 function About() {
   const engResume = "https://drive.google.com/file/d/1yWjiqpy8hogHbtwtAjI1cs08kA8kzq8S/view?usp=sharing";
@@ -29,7 +32,7 @@ function About() {
                 <ul>
                   <li><a href={webdevResume} target="_blank" rel="noopener noreferrer">My Web Development Resume</a></li>
                   <br />
-                  <li><a href={process.env.PUBLIC_URL + engResume} target="_blank" rel="noopener noreferrer">My Engineering Resume</a></li>
+                  <li><a href={/*process.env.PUBLIC_URL +*/ engResume} target="_blank" rel="noopener noreferrer">My Engineering Resume</a></li>
                 </ul>
 
                 <br />
@@ -50,28 +53,8 @@ function About() {
             </Row>
           </Col>
 
-          <Col id="about-text" size="s12 m12 l7">
-            <p>Hello there! My name is Umut Alptekin. I am currently looking for a job as either an engineer, or a Full-Stack/Back-end developer.
-          </p>
-
-            <p>I have recently graduated from Penn State University with a Bachelor of Science in Mechanical Engineering in May 2020,
-            and, shortly after, finished a 3-month long bootcamp about Full-Stack Web Development from University of Texas at Austin.
-          </p>
-
-            <p>For some background information, I was born and raised in Istanbul, Turkey. I have gone to Lycée Saint-Joseph Istanbul, a French high school, then moved to USA to attend Penn State University.
-            Currently, I am back in Istanbul for the foreseeable future, but still open to relocating to another country to chase a work opportunity.
-          </p>
-
-            <p>The bootcamp was a very interesting and constructive experience for me. I have learned so much about coding in general, project workflow in software, and about web development itself.
-            Every week, I was challenged with a new project about a new topic, and had to learn how to use the tools I was just introduced to pretty much on the job.
-            After the bootcamp, I am still learning more about the tools and technologies to better leverage them, and finding different solutions to given problems.
-          </p>
-
-            <p>While in Penn State, I loved studying engineering, since the technical problem-solving aspect gives me quite a lot of joy.
-            One of the most memorable things I was involved in was my capstone project where my teammates and I were building a gas-powered, fiberglass car with the maximum fuel efficiency as possible.
-            Our plans to use it in the Shell Eco-marathon competition could not happen, it was still an amazing experience.
-          </p>
-
+          <Col size="s12 m12 l7">
+           <Bio />
           </Col>
         </Row>
 

ae7511ca32c2745bcf187e0375484c8c1030577e
Umut Alptekin <amacalptekin@gmail.com>
2021-01-15 20:48:15 +0300

    react navbar links changed to navlink, day-planner project is updated with corrent repo link and description

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,27 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./style.css";
 
 function Nav() {
   return (
       <nav>
         <div className="nav-wrapper row">
-          <Link to="/" id="name" className="center-align col s8 hoverable" >
+          <NavLink to="/" id="name" className="center-align col s8 hoverable" >
             <span className="flow-text">Umut A. ALPTEKIN</span>
-          </Link>
+          </NavLink>
 
           <a className="dropdown-trigger hide-on-large-only center-align col s4" href="#!" data-target="dropdown1">
             <i className="material-icons hoverable">menu</i>
           </a>
 
           <ul id="dropdown1" className="dropdown-content">
-            <li><Link to="/">About Me</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
+            <li><NavLink to="/">About Me</NavLink></li>
+            <li><NavLink to="/projects">Projects</NavLink></li>
           </ul>
 
           <ul id="navbar-pages" className="hide-on-med-and-down col s4">
-            <li><Link to="/">About Me</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
+            <li><NavLink to="/">About Me</NavLink></li>
+            <li><NavLink to="/projects">Projects</NavLink></li>
           </ul>
 
         </div>
